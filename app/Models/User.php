f35944e06ce400bfc054fe8f0ae8ce6343dff8e7
i-am-tian <sebastian.alvarezla@campusucc.edu.co>
2024-07-02 12:07:23 -0500

    functional version

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,11 +3,14 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable;
 
@@ -49,5 +52,10 @@ class User extends Authenticatable
     public function tipoUsuario()
     {
         return $this->belongsTo(TipoUsuario::class);
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->tipo_usuario_id == 1;
     }
 }
