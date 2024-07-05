47772a9fa3a2c6bf1b2e2bc9db7cc8eb083c9da0
mariab432 <maria.alvisb@campusucc.edu.co>
2024-07-05 11:07:24 -0500

    categoriafrom

--- app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
+++ app/Filament/Resources/CategoryResource/Pages/CreateCategory.php
@@ -3,10 +3,36 @@
 namespace App\Filament\Resources\CategoryResource\Pages;
 
 use App\Filament\Resources\CategoryResource;
+use App\Models\Category;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class CreateCategory extends CreateRecord
 {
     protected static string $resource = CategoryResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['created_by'] = auth()->id();
+        return $data;
+    }
+
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $categoria = Category::create($data);
+
+
+        $categoria = Category::create([
+            'nombre' => $data['nombre'],
+            'descripcion' => $data['descripcion'],
+        ]);
+
+        return $categoria;
+    }
+    
 }
