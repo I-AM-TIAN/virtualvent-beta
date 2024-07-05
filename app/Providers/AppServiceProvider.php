fc6c78f3d4b6945915f51cdc73e4c99bd3b850d6
mariab432 <maria.alvisb@campusucc.edu.co>
2024-07-05 09:00:59 -0500

    traduccion de paneles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use BezhanSalleh\FilamentLanguageSwitch\LanguageSwitch;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LanguageSwitch::configureUsing(function (LanguageSwitch $switch) {
+            $switch
+                ->locales(['ar','en','fr','es'])// also accepts a closure
+                ->visible(outsidePanels: true);
+        });
     }
 }
